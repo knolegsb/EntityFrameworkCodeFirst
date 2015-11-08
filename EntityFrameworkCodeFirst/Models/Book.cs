d328c96c2d2b4117b7d4421372c80da69a200be4
knolegsb <seon2you@hotmail.com>
2015-11-08 01:12:12 -0800

    start and added model

---
+++ EntityFrameworkCodeFirst/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkCodeFirst.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        [MaxLength(200)]
+        public string Title { get; set; }
+        [MaxLength]
+        public string Description { get; set; }
+        [MaxLength(20)]
+        public string ISBN { get; set; }
+        public DateTime DatePublished { get; set; }
+        public Category Category { get; set; }
+    }
+}
