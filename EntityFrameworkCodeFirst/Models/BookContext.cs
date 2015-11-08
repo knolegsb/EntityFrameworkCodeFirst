05cd33cf802a45270478fb75cf2352c9258b56ec
knolegsb <seon2you@hotmail.com>
2015-11-08 01:24:01 -0800

    added book context

--- EntityFrameworkCodeFirst/Models/BookContext.cs
+++ EntityFrameworkCodeFirst/Models/BookContext.cs
@@ -8,6 +8,8 @@ namespace EntityFrameworkCodeFirst.Models
 {
     public class BookContext : DbContext
     {
-        
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
